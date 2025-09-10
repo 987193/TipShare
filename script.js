e481620e37530882aec887d6491f957a137ae072
987193 <jindalk198@gmail.com>
2025-09-10 11:14:46 +0530

    completed js

---
+++ script.js
@@ -0,0 +1,21 @@
+let Amount=document.getElementById("Amount");
+let Persons=document.getElementById("Persons");
+let Tip=document.getElementById("Tip");
+let button=document.getElementById("SubmitBtn");
+let Result=document.getElementById("Result");
+
+function compute(){
+    if(Amount.value<=0 || Persons.value<=0 ){
+        Result.innerHTML=`Invalid Inputs`;
+        return;
+    }
+    console.log(Amount.value);
+    console.log(Persons.value);
+    console.log(Tip.value);
+    
+    
+    
+    let TotalAmount=Number(Amount.value)+(Number(Amount.value)*Number(Tip.value)/100);
+    Result.innerHTML=`Each person must pay :$ ${TotalAmount/Number(Persons.value)}`;
+}
+button.addEventListener('click',compute);
